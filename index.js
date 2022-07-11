f475cae9af72d24c709f519a247ab48ebcc3745e
swathi-srec <67637928+swathi-srec@users.noreply.github.com>
2022-07-11 22:53:41 +0530

    My first commit!
    My first commit!

---
+++ index.js
@@ -0,0 +1,10 @@
+function openCity(cityName) {
+  var i;
+  var x = document.getElementsByClassName("tab");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  document.getElementById(cityName).style.display = "block";
+}
+
+
